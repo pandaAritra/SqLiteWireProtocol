83aff61cc689214d213de4f37e9c37f1e2a4b781
aritra panda <pandaaritra76@gmail.com>
2026-03-23 01:39:59 +0530

    add go.mod and go.sum for dependency management; implement db package with Open and Query functions

---
+++ db/db.go
@@ -0,0 +1,18 @@
+package db
+
+import (
+	"database/sql"
+
+	_ "modernc.org/sqlite"
+)
+
+func Open(path string) (*sql.DB, error) {
+	return sql.Open("sqlite", path)
+}
+
+func Query(db *sql.DB, qString string) (*sql.Rows, error) {
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+	return db.Query("")
+}
